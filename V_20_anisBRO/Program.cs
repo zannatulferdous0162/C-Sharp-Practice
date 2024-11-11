4029285e0549a40182657bdacdf862137b71a2b3
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-11-11 21:00:25 +0600

    Complite 24 c sharp playlist

--- V_20_anisBRO/Program.cs
+++ V_20_anisBRO/Program.cs
@@ -10,32 +10,72 @@ namespace V_20_anisBRO
     {
         static void Main(string[] args)
         {
-            int number1,number2, number3,sum;
+            /*
+            int number1, number2, number3, sum;
             double average;
             Console.Write("number1 = ");
             number1 = Convert.ToInt32(Console.ReadLine());
-
             Console.Write("number2 = ");
             number2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("number2 = ");
-            number2 = Convert.ToInt32(Console.ReadLine());
-
             Console.Write("number3 = ");
             number3 = Convert.ToInt32(Console.ReadLine());
-
             sum = number1 + number2 + number3;
             Console.WriteLine($"sum = {sum}");
-
             average = (double)sum / 3;
-            Console.WriteLine($"average = {average.ToString("F2")}");
+            Console.WriteLine($"average = {average}");
 
+            
             //vedio 21 
             //triangle = 0.5 * base * height
 
             double baseArea, heightArea, traingleArea;
-            Console.WriteLine("")
+            Console.WriteLine("Traingle area calculayor :");
 
-                //
+            Console.Write("Base = ");
+            baseArea = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Height");
+            heightArea = Convert.ToDouble(Console.ReadLine());
+
+            traingleArea = 0.5 * baseArea * heightArea;
+            Console.WriteLine($"Traingle Area = {traingleArea.ToString("F2")}");
+
+            
+            //vedio 22
+            //circle area = 3.1416 * radious * radious
+
+            double radious,circleArea;
+            Console.WriteLine("circle area Calculator");
+            Console.WriteLine("Radious");
+            radious = Convert.ToDouble(Console.ReadLine());
+
+
+            circleArea = 3.1416 * radious * radious;
+            Console.WriteLine($"Circle Area = {circleArea.ToString("F2")}");
+
+            //Vedio 23
+
+            double fahrenheit, celcious;
+            Console.WriteLine("Fahrenheit Calculation :");
+            Console.WriteLine("Fahrenheit :");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            celcious = (fahrenheit - 32) / 1.8;
+            Console.WriteLine($"Celcious = {celcious:f2} Degrees");
+             
+
+            //Vedio 24
+            double fahrenheit, celcious;
+            Console.WriteLine("Celcious Calculation :");
+            Console.Write("Celcious  :");
+            celcious = Convert.ToDouble(Console.ReadLine());
+
+            fahrenheit = (1.8 * celcious) + 32;
+            Console.WriteLine($"Fahrenheit = {fahrenheit:F2} Degress");
+            */
+
+
+
         }
     }
 }
