194f6ddfb8ca68ed77c9aba7bba9732b51d8a495
zannatulferdous0162 <zannatulferdous0162@.com>
2024-11-20 04:48:05 +0600

    add calculator convertoradd temparature project

---
+++ Calculator_convertor/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_convertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            char operation;
+
+            Console.Write("Enter an operation (+,-,*,/) :");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter an number1 input");
+            number1 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter an number2 input");
+            number2 = Convert.ToChar(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    {
+                        Console.WriteLine($"{number1} + {number2} = {number1 + number2} ");
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.WriteLine($"{number1} - {number2} = {number1 - number2} ");
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.WriteLine($"{number1} * {number2} = {number1 * number2} ");
+                        break;
+                    }
+                case '/':
+                    {
+                        if (number2 != 0)
+                        {
+                            Console.WriteLine($"{number1} / {number2} = {number1 / number2} ");
+   
+                        }
+                        else
+                        {
+                            Console.WriteLine("Can not divided by ZERO");
+                        }
+                        break;
+                    }
+                   
+                default:
+                        Console.WriteLine("Invalid");
+                        break;
+                    
+            }
+
+        }
+    }
+}
---
+++ project 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temperature converted started");
+            Console.WriteLine("Choose 1 : Fahrenheit to celsisus");
+            Console.WriteLine("Choose 2 : Celsisus to Fahrenheit");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        Console.Write("Enter Fahrenheit temparature :");
+                        double fahrenheit = Convert.ToDouble(Console.ReadLine());
+                        double celsisus = (fahrenheit - 32) / 1.8;
+                        Console.Write($"Temparature in  celsisus: {celsisus:F2} ");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write("Enter Celsisus temparature :");
+                        double cels = Convert.ToDouble(Console.ReadLine());
+                       
+                        double fahren = cels * 1.8 + 32;
+                        Console.Write($"Temparature in  celsisus: {fahren:F2} ");
+                        break;
+                    }
+                   
+                default:
+                    break;
+            }
+        }
+    }
+}
