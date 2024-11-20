f4875888c80708a6101db23e9fc41ce1285713b1
zannatulferdous0162 <zannatulferdous0162@.com>
2024-11-21 05:27:26 +0600

    add multiplication_table_from_m_to_n

---
+++ factorial of a number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial_of_a_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int factorial = 1;
+            for(int i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine($"Factorial({number} = {factorial})");
+        }
+    }
+}
---
+++ multiplication table from m to n/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication_table_from_m_to_n
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber, endNumber;
+            Console.Write("Enter startNumber number : ");
+            startNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter endNumber number : ");
+            endNumber = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = startNumber; i <= endNumber; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} X {j} = {i * j}");
+                }
+                Console.WriteLine("  ");
+            }
+        }
+    }
+}
